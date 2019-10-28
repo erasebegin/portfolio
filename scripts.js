82a5ef42efef971684daf7322a4d29fa359e3ac7
erasebegin <chris.james.haupt@gmail.com>
2019-10-28 21:31:46 +0100

    added cards, code logos, changes many properties

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function go_to_projects() {
+
+}
+
+// $('a[href*="#projects"]').on('click', function(e) {
+//     e.preventDefault()
+  
+//     $('html, body').animate(
+//       {
+//         scrollTop: $($(this).attr('href')).offset().top,
+//       },
+//       500,
+//       'linear'
+//     )
+//   })
+
+// https://codepen.io/taniarascia/pen/MJEXZj
